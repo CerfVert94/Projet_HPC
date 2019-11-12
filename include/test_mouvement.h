60626cc0afca5978aa78a3401481f6fc6bf5a281
antoinesomsay <antoine.somsay@gmail.com>
2019-11-12 13:50:07 +0100

    fast

--- include/test_mouvement.h
+++ include/test_mouvement.h
@@ -3,6 +3,9 @@
 /* ------------------------ */
 
 
-void test_corps_boucle_SigmaDelta_step0();
 void test_corps_boucle1_SigmaDelta_step1();
+void test_corps_boucle2_SigmaDelta_Step1();
+void test_corps_boucle3_SigmaDelta_Step1();
+void test_corps_boucle4_SigmaDelta_Step1();
+
 void all_test();
--- src/test_mouvement.c
+++ src/test_mouvement.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
 #include "nrdef.h"
 #include "nrutil.h"
 #include "util.h"
@@ -7,45 +9,34 @@
 #include "mouvement.h"
 #include "test_mouvement.h"
 
-void test_corps_boucle_SigmaDelta_step0() {
-
-	uint8 a; // t0->V[i][j]
-	a = 1;
-	if (a == 1) PASS();
-	else FAIL();
-
-}
-
 void test_corps_boucle1_SigmaDelta_step1() {
 
 	int pass = 1;
-	uint8 t_1M = 300; // t_1->M[i][j]
+	uint8 test;
+	uint8 t_1M = 132; // t_1->M[i][j]
 	uint8 tI = 86; // t->I[i][j]
-	uint8 tM = 56; // t->M[i][j]
-	printf("%d %d\n%d %d\n", t_1M, tI, t_1M-128, tI-128);
-	if ((t_1M-128) > (tI-128)) {
-		tM = t_1M - 1;	
-	}
-	if (tM != t_1M - 1) pass = 0;
+	uint8 tM = 131; // t->M[i][j]
+
+	if (t_1M > tI)
+		test = t_1M - 1;
+	if (tM != test) pass = 0;
 
 
 	t_1M = 86; // t_1->M[i][j]
 	tI = 132; // t->I[i][j]
-	tM = 56; // t->M[i][j]
+	tM = 87; // t->M[i][j]
 
-	if (t_1M < tI) {
-		tM = t_1M + 1;
-		if (tM != t_1M + 1) pass = 0;
-	}
+	if (t_1M < tI)
+		test = t_1M + 1;
+	if (tM != test) pass = 0;
 
 	t_1M = 86; // t_1->M[i][j]
 	tI = 86; // t->I[i][j]
-	tM = 56; // t->M[i][j]
+	tM = 86; // t->M[i][j]
 
-	if (t_1M == tI) {
-		tM = t_1M;
-		if (tM != t_1M) pass = 0;
-	}
+	if (t_1M == tI)
+		test = t_1M;
+	if (tM != test) pass = 0;
 
 	if (pass) PASS();
 	else FAIL();
@@ -56,12 +47,39 @@ void test_corps_boucle2_SigmaDelta_Step1() {
 
 	int pass = 1;
 
+	uint8 test;
+	uint8 tM = 86;
+	uint8 tI = 56;
+	uint8 t0 = 30;
+
+	test = abs(tM - tI);
+	if (t0 != test) pass = 0;
+
+	test = abs(tI - tM);
+	if (t0 != test) pass = 0;
+
+	if (pass) PASS();
+	else FAIL();
+
 }
 
+void test_corps_boucle3_SigmaDelta_Step1() {
+
+	int pass = 1;
+
+	uint8 test;
+	uint8 t_1V = 132;
+	uint8 t0 = 86;
+	uint8 tV 133;
+
+	/* N = 2 dans mouvement.h */
+	if (t_1V < (N * ))
+
+}
 
 void all_test() {
 
-	test_corps_boucle_SigmaDelta_step0();
 	test_corps_boucle1_SigmaDelta_step1();
+	test_corps_boucle2_SigmaDelta_Step1();
 
 }
