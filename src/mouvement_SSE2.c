6337a4cc5dce5f73c4d44982eb41d37264f3bd04
Antoine Somsay <somsay@MacBook-Pro-de-Antoine.local>
2020-01-24 17:28:22 +0100

    some advance

--- src/mouvement_SSE2.c
+++ src/mouvement_SSE2.c
@@ -34,7 +34,7 @@ void SigmaDelta_step1_SSE(p_vimage vt, p_vimage vt_1) {
 			tI   = _mm_load_si128((vuint8*) &vt->I[i][j]);
 			t_1I = _mm_load_si128((vuint8*) &vt_1->I[i][j]);
 
-			vec_cmplsb(tI, t_1I, c, S);
+			vec_cmplsb(tI, t_1I, c, SUB);
 			
 
 
@@ -61,6 +61,33 @@ void SigmaDelta_step2_SSE(p_vimage vt) {
 			vsub = vec_subabs(tI, tM);
 
 			_mm_store_si128((vuint8*) &vt->O[i][j], vsub);
+		}
+	}
+	
+}
+
+/*----------------------------------*/
+void SigmaDelta_step4_SSE(p_vimage vt) {
+/*----------------------------------*/
+
+	vuint8 tO, tV, tE;
+
+	vuint8 c, MSB = init_vuint8(127);
+
+	for(int i = vt->nrl+BORD; i < vt->nrh-BORD; i++) {
+		for(int j = vt->v0+1; j < vt->v1-1; j++) {
+			/*tO = _mm_load_si128((vuint8*) &vt->O[i][j]);
+			tV = _mm_load_si128((vuint8*) &vt->V[i][j]);
+			
+			t0 = _mm_sub_epi8(t0, MSB);
+			tV = _mm_sub_epi8(tV, MSB);
+			c  = _mm_cmplt_epi8(t0, tV);
+			t0 = _mm_add_epi8(t0, MSB);
+			tV = _mm_add_epi8(tV, MSB);*/
+
+			
+
+
 		}
 	}
 	
