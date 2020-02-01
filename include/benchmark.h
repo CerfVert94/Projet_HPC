8f74c496da9b589461d56d1b813ef050c51c7774
Roqyun Ko <roqyun.ko@gmail.com>
2020-02-01 07:31:11 +0100

    sd benchmakr

--- include/benchmark.h
+++ include/benchmark.h
@@ -9,12 +9,12 @@ unsigned long long get_min_cpu_cycles_of_morpho       (struct morpho_set *ptr_ms
 unsigned long long get_cpu_cycles_of_packed_morpho    (struct morpho_set *ptr_mset                  , uint8 **X,  long nrl, long nrh, long ncl, long nch, uint8 **temp_buffer, uint8 **Y);
 unsigned long long get_min_cpu_cycles_of_packed_morpho(struct morpho_set *ptr_mset, long packet_size, uint8 **X,  long nrl, long nrh, long ncl, long nch, uint8 **temp_buffer, uint8 **Y);
 
-unsigned long long get_cpu_cycles_of_sd               (struct morpho_set *ptr_mset                  , uint8** X, uint8** Y, uint8** Z, long nrl, long nrh, long ncl, long nch);
-unsigned long long get_min_cpu_cycles_sd             (struct morpho_set *ptr_mset, long packet_size, uint8** X, uint8** Y, uint8** Z, long nrl, long nrh, long ncl, long nch);
+unsigned long long get_cpu_cycles_of_sd               (struct sd_set       *sdsets                  , uint8** X, uint8** Y, uint8** Z, long nrl, long nrh, long ncl, long nch, uint8 n_coeff, uint8 v_min, uint8 v_max);
+unsigned long long get_min_cpu_cycles_of_sd              (struct sd_set       *sdsets, long packet_size, uint8** X, uint8** Y, uint8** Z, long nrl, long nrh, long ncl, long nch, uint8 n_coeff, uint8 v_min, uint8 v_max);
 
-double **benchmark_of_morpho        (struct morpho_set *msets, long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size);
-double **benchmark_of_packed_morpho (struct morpho_set *msets, long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size);
-double **benchmark_of_sd            (struct morpho_set *msets, long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size);
+double **benchmark_of_morpho        (struct morpho_set *msets , long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size);
+double **benchmark_of_packed_morpho (struct morpho_set *msets , long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size);
+double **benchmark_of_sd            (struct sd_set     *sdsets, long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size);
 
 double **init_benchmark_results(long nb_funcs, long size, long step);
 void     free_benchmark_results(double **results, long nb_funcs);
--- src/benchmark.c
+++ src/benchmark.c
@@ -2,6 +2,7 @@
 #include <nrutil.h>
 #include <img.h>
 #include <morpho.h>
+#include <mouvement.h>
 #include <stdbool.h>
 #include <test_morpho.h>
 #include <benchmark.h>
@@ -68,9 +69,76 @@ unsigned long long get_min_cpu_cycles_of_morpho(struct morpho_set *ptr_mset, lon
 	return min_cycles;
 }
 
+unsigned long long get_cpu_cycles_of_sd(struct sd_set *sdset, uint8** X, uint8** Y, uint8** Z, long nrl, long nrh, long ncl, long nch, uint8 n_coeff, uint8 v_min, uint8 v_max)
+{
+    unsigned long long begin = 0, end = 0, cycles = 0;
+
+	begin = __rdtsc();
+	sdset->sd_func(X, Y, Z, nrl, nrh, ncl, nch, n_coeff, v_min, v_max);
+	end = __rdtsc();
+	return end - begin;
+}
+
+unsigned long long get_min_cpu_cycles_of_sd(struct sd_set *sdset, long packet_size, uint8** X, uint8** Y, uint8** Z, long nrl, long nrh, long ncl, long nch, uint8 n_coeff, uint8 v_min, uint8 v_max)
+{
+	unsigned long long min_cycles, *cycles;
+	int i;
+	if(packet_size <= 0) {
+		fprintf(stderr, "Error : the size of a packet should be at least 1.\n");
+		exit(EXIT_FAILURE);
+	}
+	cycles = (unsigned long long *) malloc(sizeof(unsigned long long) * packet_size);
+	for (int i = 0; i < packet_size; i++) {
+		cycles[i] = get_cpu_cycles_of_sd(sdset, X, Y, Z, nrl, nrh, ncl, nch, n_coeff, v_min, v_max);
+	}
+
+	min_cycles = cycles[0];
+	for (long i = 1; i < packet_size; i++) 
+		if (min_cycles > cycles[i]) 
+			min_cycles = cycles[i];
+	free(cycles);
+	return min_cycles;
+}
 
 
 
+double **benchmark_of_sd(struct sd_set     *sdsets, long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size)
+{
+	unsigned long long  min_cycles_sum, begin, end;
+	long size, idx_set, idx_test, nrl, nrh, ncl, nch, cnt = 0;
+	double **results;
+	uint8 **X, **Y, **Z;
+	const int n_coeff = 2;
+	const int v_min = 1;
+	const int v_max = 254;
+	
+	// Initialize to save the benchmark result.
+	results = init_benchmark_results(nb_sets, (hs - ls)  + 1, step);
+	
+	cnt = 0;
+	for (size = ls - 1; size < hs; size += step) {
+		X = ui8matrix_checker(0, size, 0, size, 3, 1); 
+		Y = ui8matrix_checker(0, size, 0, size, 3, 0); 
+		Z = ui8matrix        (0, size, 0, size); 
+		
+		for (idx_set = 0; idx_set < nb_sets; idx_set++) {
+			begin = __rdtsc();			
+			min_cycles_sum = 0;
+			for (idx_test = 0; idx_test < nb_tests; idx_test++)
+				min_cycles_sum += get_min_cpu_cycles_of_sd(&sdsets[idx_set], packet_size, X, Y, Z, 0, size, 0, size, N, v_min, v_max);
+			results[idx_set][cnt] = ((double)min_cycles_sum / (nb_tests * (size + 1) * (size + 1)));
+			end = __rdtsc();
+			if ((size + 1) % 500 == 0 || size >= hs - 1) 
+				printf("\t["LALIGNED_STR"] Ran SigmaDelta %d * %d times on %ld x %ld matrix during %llu cycles.\n",  sdsets[idx_set].func_name, packet_size, nb_tests, size + 1, size + 1, (end - begin));			
+		}
+	
+		free_ui8matrix(X, 0, size, 0, size);
+		free_ui8matrix(Y, 0, size, 0, size);
+		free_ui8matrix(Z, 0, size, 0, size);
+		cnt++;
+	}
+	return results;
+}
 
 double **benchmark_of_morpho(struct morpho_set *msets, long nb_sets, long ls, long hs, long step, int nb_tests, int packet_size)
 {
