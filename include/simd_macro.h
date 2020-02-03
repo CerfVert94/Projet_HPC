a04407780fa97fb6f6dc4b25fb0b4f2c57e8d909
Antoine Somsay <somsay@MacBook-Pro-de-Antoine.local>
2020-02-03 23:00:36 +0100

    fix simd morpho

--- include/simd_macro.h
+++ include/simd_macro.h
@@ -15,7 +15,7 @@
 #define leftshift1a  _mm_setr_epi8(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,-1)
 #define leftshift1b  _mm_setr_epi8(-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,0)
 #define rightshift1a _mm_setr_epi8(-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14)
-#define rightshift1b _mm_setr_epi8(0,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)
+#define rightshift1b _mm_setr_epi8(15,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1)
 
 // -------
 // operateurs
